c5d3b723efd0475e22cd55815502ffb5a1bd2e4f
FranckCV <francocor0105@gmail.com>
2024-11-03 12:32:14 -0500

    RESPALDO (inserts mejorados)

--- esquema_dae.sql
+++ esquema_dae.sql
@@ -58,8 +58,8 @@ create table DUEniO (
   id            int4 not null, 
   nombres       varchar(150) not null, 
   apePaterno    varchar(200) not null, 
-  doc_identidad varchar(20) not null, 
   apeMaterno    varchar(200) not null, 
+  doc_identidad varchar(20) not null, 
   telefono      varchar(15) not null, 
   telefonoAlt   varchar(15), 
   correo        varchar(150), 
@@ -127,8 +127,9 @@ create table CUSTODIA (
   primary key (MASCOTAid, 
   DUEniOid));
 create table DETALLE_SERVICIO (
-  servicio_id int4 not null, 
-  MEDICOid    int4 not null, 
+  servicio_id     int4 not null,
+  MEDICOid        int4 not null,
+  disponibilidad  bool not null,
   primary key (servicio_id, 
   MEDICOid));
 create table DETALLE_MEDICAMENTO (
@@ -257,49 +258,55 @@ INSERT INTO VACUNA (id, nombre, dosis_x_kgpeso, especie_id , disponibilidad) VAL
 
 
 
-
--- INSERT INTO DUEniO (numdoc, nombres, apePaterno, apeMaterno, telefono, telefonoAlt, correo, direccion, sexo, vigencia, codtipo) VALUES
---   ('71234567', 'Carlos', 'García', 'Pérez', '987654321', '912345678', 'cgarcia@example.com', 'Av. Siempre Viva 123', true, true, 1),
---   ('82765432', 'María', 'López', 'Sánchez', '976543210', NULL, 'mlopez@example.com', 'Jr. Los Pinos 456', false,true, 1),
---   ('65432189', 'José', 'Martínez', 'Guzmán', '964321098', '941234567', NULL, 'Calle Las Rosas 789', true, true, 1),
---   ('98651234', 'Ana', 'Rodríguez', 'Fernández', '932165478', NULL, 'arodriguez@example.com', 'Av. Las Palmeras 456', false, true,1),
---   ('73512368', 'Luis', 'Paredes', 'Zapata', '911234569', '998877665', 'lparedes@example.com', 'Jr. Las Lomas 234', true, true, 1),
---   ('84561234', 'Rosa', 'Chávez', 'Valverde', '945612378', NULL, 'rchavez@example.com', 'Calle Los Cedros 156', false,true, 1),
---   ('78912345', 'Pedro', 'Cruz', 'Huaman', '987123654', '954321876', 'pcruz@example.com', 'Av. Perú 789', true,true, 2),
---   ('62341234', 'Luisa', 'Ortiz', 'Pérez', '912345674', NULL, 'lortiz@example.com', 'Jr. San Juan 342', false, true, 3),
---   ('98123456', 'Miguel', 'Alvarado', 'Reyes', '987412356', '943215678', 'malvarado@example.com', 'Av. Colonial 342', true,true, 2),
---   ('81234567', 'Carmen', 'Salinas', 'Ríos', '965412378', '976543212', 'csalinas@example.com', 'Jr. Las Magnolias 765', false, true, 3);
+INSERT INTO MEDICO (id, doc_identidad, nombres, apePaterno, apeMaterno, sexo, disponibilidad, vigencia, especialidad_id) 
+VALUES 
+    (1, '12345678', 'Juan', 'Pérez', 'García',        TRUE, TRUE, TRUE, 1),
+    (2, '23456789', 'Carlos', 'Gómez', 'Sánchez',     TRUE, FALSE, FALSE, 5),
+    (3, '34567890', 'Pedro', 'Hernández', 'Ramírez',  TRUE, FALSE, TRUE, 5),
+    (4, '87654321', 'Ana', 'López', 'Martínez',       FALSE, FALSE, TRUE, 2), 
+    (5, '98765432', 'Lucía', 'Rodríguez', 'Morales',  FALSE, TRUE, TRUE, 4);
 
 
--- INSERT INTO MASCOTA (id, nombre, fecha_nacimiento, altura, peso, notaAdicional, sexo, esterilizado, desparasitado, estado_salud, raza_id, especie_id, fecha_registro, estado) VALUES
---   (1, 'Luna', '2020-05-15', 35.50, 12.30, 'Tiene mucha energía y es muy activa.', false, true, true, 'S', 1, 1, '2020-05-15', true),
---   (2, 'Max', '2018-11-22', 45.20, 18.75, 'Le encanta correr largas distancias y es muy fuerte.', true, true, true, 'S', 2, 1, '2020-11-22', true),
---   (3, 'Bella', '2019-09-30', 38.10, 15.60, 'Padece artritis crónica, bajo tratamiento.', false, false, true, 'C', 3, 2, '2021-10-07', true),
---   (4, 'Simba', '2011-07-05', 50.00, 25.00, 'Un poco tímido al principio, pero muy juguetón.', true, false, false, 'S', 1, 1, '2020-12-07', true),
---   (5, 'Rocky', '2017-03-01', 42.50, 20.40, 'Sufre de insuficiencia renal crónica, requiere dieta especial.', true, true, true, 'C', 2, 1, '2017-10-07', true),
---   (6, 'Coco', '2022-01-10', 30.80, 10.20, 'Muy cariñosa con los niños y social.', false, true, true, 'S', 8, 4, '2022-01-10', true),
---   (7, 'Nala', '2016-06-18', 37.00, 14.50, 'Cáncer avanzado, en tratamiento paliativo.', false, false, false, 'T',10, 5, '2016-06-18', true),
---   (8, 'Charlie', '2020-12-09', 48.30, 22.10, 'Amigable con otras mascotas, le gusta nadar.', true, true, true, 'S', 2, 1, '2020-12-09', true),
---   (9, 'Milo', '2019-02-25', 39.20, 16.80, 'Alergia crónica a ciertos alimentos.', true, false, true, 'C', 1, 1, '2019-02-25', true),
---   (10, 'Chloe', '2015-10-28', 33.50, 11.90, 'Cáncer terminal, con pocos meses de vida.', false, true, true, 'T', 7, 4, '2015-10-28', true);
+INSERT INTO DUEniO (id, doc_identidad, nombres, apePaterno, apeMaterno, telefono, telefonoAlt, correo, direccion, sexo, vigencia) VALUES
+  (1, '71234567', 'Carlos', 'García', 'Pérez', '987654321', '912345678', 'cgarcia@example.com', 'Av. Siempre Viva 123', true, true),
+  (2, '82765432', 'María', 'López', 'Sánchez', '976543210', NULL, 'mlopez@example.com', 'Jr. Los Pinos 456', false,true),
+  (3, '65432189', 'José', 'Martínez', 'Guzmán', '964321098', '941234567', NULL, 'Calle Las Rosas 789', true, true),
+  (4, '98651234', 'Ana', 'Rodríguez', 'Fernández', '932165478', NULL, 'arodriguez@example.com', 'Av. Las Palmeras 456', false, true),
+  (5, '73512368', 'Luis', 'Paredes', 'Zapata', '911234569', '998877665', 'lparedes@example.com', 'Jr. Las Lomas 234', true, true),
+  (6, '84561234', 'Rosa', 'Chávez', 'Valverde', '945612378', NULL, 'rchavez@example.com', 'Calle Los Cedros 156', false,true),
+  (7, '78912345', 'Pedro', 'Cruz', 'Huaman', '987123654', '954321876', 'pcruz@example.com', 'Av. Perú 789', true,true),
+  (8, '62341234', 'Luisa', 'Ortiz', 'Pérez', '912345674', NULL, 'lortiz@example.com', 'Jr. San Juan 342', false, true),
+  (9, '98123456', 'Miguel', 'Alvarado', 'Reyes', '987412356', '943215678', 'malvarado@example.com', 'Av. Colonial 342', true,true),
+  (10, '81234567', 'Carmen', 'Salinas', 'Ríos', '965412378', '976543212', 'csalinas@example.com', 'Jr. Las Magnolias 765', false, true);
+
+
+INSERT INTO MASCOTA (id, nombre, fecha_nacimiento, altura, peso, notaAdicional, sexo, esterilizado, desparasitado, estado_salud, raza_id, vigencia) VALUES
+  (1, 'Luna', '2020-05-15', 35.50, 12.30, 'Tiene mucha energía y es muy activa.',                            false, true, true, 'S', 1,   true),
+  (2, 'Max', '2018-11-22', 45.20, 18.75, 'Le encanta correr largas distancias y es muy fuerte.',             true, true, true, 'S', 2,    true),
+  (3, 'Bella', '2019-09-30', 38.10, 15.60, 'Padece artritis crónica, bajo tratamiento.',                     false, false, true, 'C', 3,  true),
+  (4, 'Simba', '2011-07-05', 50.00, 25.00, 'Un poco tímido al principio, pero muy juguetón.',                true, false, false, 'S', 1,  true),
+  (5, 'Rocky', '2017-03-01', 42.50, 20.40, 'Sufre de insuficiencia renal crónica, requiere dieta especial.', true, true, true, 'C', 2,    true),
+  (6, 'Coco', '2022-01-10', 30.80, 10.20, 'Muy cariñosa con los niños y social.',                            false, true, true, 'S', 8,   true),
+  (7, 'Nala', '2016-06-18', 37.00, 14.50, 'Cáncer avanzado, en tratamiento paliativo.',                      false, false, false, 'T',10, false),
+  (8, 'Charlie', '2020-12-09', 48.30, 22.10, 'Amigable con otras mascotas, le gusta nadar.',                 true, true, true, 'S', 2,    true),
+  (9, 'Milo', '2019-02-25', 39.20, 16.80, 'Alergia crónica a ciertos alimentos.',                            true, false, true, 'C', 1,   true),
+  (10, 'Chloe', '2015-10-28', 33.50, 11.90, 'Cáncer terminal, con pocos meses de vida.',                     false, true, true, 'T', 7,   true);
+
+
+INSERT INTO usuario (codUsuario, nomUsuario, estado, sexo, clave, nombres, apPaterno, apMaterno, cargo) VALUES
+(1, '123', TRUE, TRUE, '123', 'Francisco', 'Hernandez', 'Gomez','A'),
+(2, 'lrodriguez', TRUE, FALSE, 'clave_segura2', 'Laura', 'Rodriguez', 'Perez','A'),
+(3, 'jmendoza', FALSE, TRUE, 'clave_segura3', 'Javier', 'Mendoza', 'Ramirez','E'),
+(4, 'ccruz', TRUE, FALSE, 'clave_segura4', 'Claudia', 'Cruz', 'Lopez','E'),
+(5, 'rlopez', TRUE, TRUE, 'clave_segura5', 'Ricardo', 'Lopez', 'Diaz','V');
 
 
 
 
--- INSERT INTO MEDICO (dni, nombres, apePaterno, apeMaterno, sexo, disponibilidad, vigencia, especialidad_id) 
--- VALUES 
---     ('12345678', 'Juan', 'Pérez', 'García',        TRUE, TRUE, TRUE, 1),
---     ('23456789', 'Carlos', 'Gómez', 'Sánchez',     TRUE, FALSE, FALSE, 5),
---     ('34567890', 'Pedro', 'Hernández', 'Ramírez',  TRUE, FALSE, TRUE, 5),
---     ('87654321', 'Ana', 'López', 'Martínez',       FALSE, FALSE, TRUE, 2), 
---     ('98765432', 'Lucía', 'Rodríguez', 'Morales',  FALSE, TRUE, TRUE, 4);
 
 
--- INSERT INTO usuario (codUsuario, nomUsuario, estado, sexo, clave, nombres, apPaterno, apMaterno, cargo) VALUES
--- (1, '123', TRUE, TRUE, '123', 'Francisco', 'Hernandez', 'Gomez','A'),
--- (2, 'lrodriguez', TRUE, FALSE, 'clave_segura2', 'Laura', 'Rodriguez', 'Perez','A'),
--- (3, 'jmendoza', FALSE, TRUE, 'clave_segura3', 'Javier', 'Mendoza', 'Ramirez','E'),
--- (4, 'ccruz', TRUE, FALSE, 'clave_segura4', 'Claudia', 'Cruz', 'Lopez','E'),
--- (5, 'rlopez', TRUE, TRUE, 'clave_segura5', 'Ricardo', 'Lopez', 'Diaz','V');
 
---select * from medicamento;
+
+
+
+
